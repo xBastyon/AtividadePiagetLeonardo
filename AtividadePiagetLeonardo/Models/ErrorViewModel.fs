0c5775c76bea301014721671dc30b41f4b779ee6
xBastyon <leonardozx10@yahoo.com.br>
2025-03-17 19:27:26 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadePiagetLeonardo/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace AtividadePiagetLeonardo.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
---
+++ PiagetLeonardo/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿namespace PiagetLeonardo.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Escola { get; set; }
+        public string? Pai { get; set; }
+        public string? Mae { get; set; }
+        public string? RGM { get; set; }
+        public DateTime Idade { get; set; }
+        public decimal Notas { get; set; }
+        public string? Professor { get; set; }
+    }
+}
